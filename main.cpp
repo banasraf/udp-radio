f63f0bac81a62d821e28b8c05880fc773f43b984
Rafał Banaś <Banas.Rafal97@gmail.com>
2018-04-27 17:13:19 +0200

    qf

--- main.cpp
+++ main.cpp
@@ -5,7 +5,6 @@ using namespace std;
 
 
 void telnetServer() {
-    auto telnetCmdHandler = InteractiveCmdHandler();
     TcpListener tcpListener(10001);
     auto net_stream = tcpListener.acceptClient();
     serve_client(net_stream);
